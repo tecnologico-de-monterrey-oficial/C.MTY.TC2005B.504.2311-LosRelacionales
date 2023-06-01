e57a5963256ef4d3e52ec2790f97a00a865b61c2
soosw <A01625514@tec.mx>
2023-06-01 10:41:05 -0600

    Jun1

---
+++ los-relacionales/src/components/NavigationBar.jsx
@@ -0,0 +1,27 @@
+import { Link } from 'react-router-dom';
+import './NavigationBar.css';
+
+function NavigationBar() {
+    return (
+        <Navbar className="NavBar" variant="dark">
+        <Container>
+          <Navbar.Brand as={Link} to='/'>
+          <img
+              alt=""
+              src="/logo.png"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Link className="Link" to="/">Inicio</Link>
+            <Link className="Link" to="/">Testimonios</Link>
+            <Link className="Link" to="/">Contacto</Link>
+            <Link className="Link" to="/">Acceder a mi cuenta</Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    );
+}
+
+export default NavigationBar;
---
+++ los-relacionales/src/components/Testimonios.jsx
@@ -0,0 +1,9 @@
+function Testimonios() {
+    return (
+      <div className="titulo">
+        <h1>Testimonios</h1>
+      </div>
+    );
+  }
+  
+  export default Testimonios;
