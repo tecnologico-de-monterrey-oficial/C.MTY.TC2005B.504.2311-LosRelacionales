190af9792ee8a44f23c624fa276d29646ed7f625
FernandoBurgos <A01236284@tec.mx>
2023-06-14 12:30:18 -0600

    correccion en index

--- los-relacionales/src/store/index.js
+++ los-relacionales/src/store/index.js
@@ -31,8 +31,38 @@ export {
 };
 
 export { useFetchPamsQuery, useAddPamMutation, useEditPamMutation, useDeletePamMutation } from "./apis/pamsApi";
+
 export {
     useFetchPersonsQuery, useFetchPersonByIdQuery, useFetchPersonsByRoleIdQuery, useFetchPersonByEmailQuery,
-    useAddPersonMutation, useEditPersonMutation, useDeletePersonMutation
+    useAddPersonMutation, useEditPersonMutation, useDeletePersonMutation, useFetchPersonByNameQuery,
+    useFetchPersonByGenderId
 } from "./apis/personsApi";
+
+export { useFetchAnswerByIdQuery, useFetchAnswerByQuestionIdQuery,
+    useAddAnswerMutation, useEditAnswerMutation, useDeleteAnswerMutation } from "./apis/answerApi";
+
+export { useFetchDimensionsQuery, useFetchDimensionByIdQuery,
+    useAddDimensionMutation, useEditDimensionMutation, useDeleteDimensionMutation } from "./apis/dimensionApi";
+
+export { useFetchGroupTypesQuery, useFetchGroupTypeByIdQuery, useFetchGroupTypeByParentIdQuery,
+    useAddGroupTypeMutation, useEditGroupTypeMutation, useDeleteGroupTypeMutation } from "./apis/groupTypeApi";
+
+    export { useFetchHealthDataByPamIdQuery, useAddHealthDataMutation,
+        useEditHealthDataMutation, useDeleteHealthDataMutation } from "./apis/healthDataApi";
+
+export { useFetchPamGroupsQuery, useFetchPamGroupByIdQuery, useFetchPamGroupByParentIdQuery,
+    useAddPamGroupMutation, useEditPamGroupMutation, useDeletePamGroupMutation } from "./apis/pamGroupApi";
+
+export { useFetchPamTestAnswerByIdQuery, useFetchPamTestAnswerByTestIdQuery,
+    useAddPamTestAnswerMutation, useEditPamTestAnswerMutation, useDeletePamTestAnswerMutation } from "./apis/pamTestAnswerApi";
+
+    export { useFetchQuestionsQuery, useFetchQuestionByIdQuery, useFetchQuestionByTestIdQuery,
+        useAddQuestionMutation, useEditQuestionMutation, useDeleteQuestionMutation } from "./apis/questionApi";
+
+export { useFetchTestsQuery, useFetchTestByIdQuery, useFetchTestByDimensionIdQuery,
+    useAddTestMutation, useEditTestMutation, useDeleteTestMutation } from "./apis/testApi";
+
+export { useFetchTestInstructionsQuery, useFetchTestInstructionByIdQuery,
+    useAddTestInstructionMutation, useEditTestInstructionMutation, useDeleteTestInstructionMutation } from "./apis/testInstructionsApi";
+
 export { signUpWithGoogle } from "./thunks/authThunk";
