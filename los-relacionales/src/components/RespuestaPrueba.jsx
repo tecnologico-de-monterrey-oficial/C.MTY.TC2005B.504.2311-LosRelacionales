19260f9966e114031ce80b55084c08ecbe73b30d
soosw <A01625514@tec.mx>
2023-06-15 19:17:54 -0600

    xd

--- los-relacionales/src/components/RespuestaPrueba.jsx
+++ los-relacionales/src/components/RespuestaPrueba.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useFetchAnswerByQuestionIdQuery } from "../store";
 import { useFetchQuestionByTestIdQuery } from "../store";
+import { useFetchTestWeightByIdQuery } from "../store";
 
 function RespuestaPrueba() {
 const {data: answerData, isFetching, isError } = useFetchAnswerByQuestionIdQuery(id)
 const [answer, setAnswer] = React.useState(null);
+const [questionsArray, setQuestionsArray] = React.useState(null);
 
 const {data: questionData, isFetching: isFetchingQuestion, isError: isErrorQuestion} = useFetchQuestionByTestIdQuery(id);
 const [question, setQuestion] = React.useState(null);
+
+const {data: weightData, isFetching: isFetchingWeight, isError: isErrorWeight} = useFetchTestWeightByIdQuery(id);
+const [weight, setWeight] = React.useState(null);
+
+useEffect(() => {
+    if (questionData) {
+        setQuestion(questionData.question[0]);
+    }
+}, [questionData]);
+
+useEffect(() => {
+    if (weightData) {
+        setWeight(weightData.weight[0]);
+    }
+}, [weightData]);
 
 useEffect(() => {
     if (answerData) {
@@ -23,6 +40,11 @@ useEffect(() => {
     <div className="AnswerPrueba">
         {answer && answer.answer && (
         <h1>RespuestaPrueba</h1>)}
+    </div>
+    <div className="Weight">
+        {weight && weight.weight && (
+            <p>Peso</p>)}
+
     </div>
     </div>
   );
