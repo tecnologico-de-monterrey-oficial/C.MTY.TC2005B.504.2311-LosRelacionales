1ce374b581892a0ffa606702b9f0495b3a03ff84
soosw <A01625514@tec.mx>
2023-06-17 01:35:49 -0600

    XD

--- los-relacionales/src/pages/pruebas/Prueba.jsx
+++ los-relacionales/src/pages/pruebas/Prueba.jsx
@@ -4,7 +4,7 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { Link } from 'react-router-dom';
 import { useAddPamTestMutation } from '../../store';
-// import Axios from 'axios';
+import Axios from 'axios';
 
 function PruebaGijon() {
 
